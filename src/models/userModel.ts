c2373aec8d98e80aa90cce04e06da35c10f1dff7
makisoflagos <amaka@fincra.com>
2025-02-17 19:11:00 +0100

    My first commit

---
+++ src/models/userModel.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+export interface IUser extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+  otp: string | null;
+  otpExpires: Date | null;
+  isVerified: boolean;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  otp: { type: String, default: null},
+  otpExpires: { type: Date, default: null},
+  isVerified: { type: Boolean, default: false }
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(password: string) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+
+export default mongoose.model<IUser>('User', userSchema);
